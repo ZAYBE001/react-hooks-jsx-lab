98b32a79c6dcf4b82ff848096d1ca426eb49ac57
zaybe <zaybe.maliq@student.moringaschool.com>
2025-04-11 11:29:22 +0300

    add new files

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,13 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return <div id="about">
+    <h2>About Me</h2>
+    <p>
+      I am a web developer with a passion for creating beautiful and functional user interfaces.
+      I enjoy learning new technologies and improving my skills.</p>
+      <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
+  </div>;
 }
 
 export default About;
